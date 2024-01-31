20ae757a018c2fb7e8fe78c306df4b38f82c37e7
Aergiaaa <125899328+Aergiaaa@users.noreply.github.com>
2024-01-31 21:34:43 +0700

    making little setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,93 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="m-0 p-0 box-border outline-none no-underline scroll-smooth bg-[var(--bg)] text-white">
+      <div
+        id="navbar"
+        className="flex justify-between items-center bg-[rgba(1,1,1,0.8)] top-0 right-0 left-0 z-[9999] fixed border-b-[1px solid #513c28] "
+      >
+        <Link href="#" className=" font-[700] size-[2rem] text-white italic">
+          Kopi <span className=" text-[var(--primary)]">Sma</span>.
+        </Link>
+        <nav className="text-white inline-block size-[1.2rem] m-[0 1rem] hover:text-[var(--primary)] after:block after:pb-[0.5rem] after:border-b[0.1rem solid var(--primary)] after:scale-x-0 after:transition-[0.2s linear] hover:after:scale-x-[0.5]">
+          <Link href="#tentang">Tentang</Link>
+          <Link href="#menu">Menu</Link>
+          <Link href="#tujuan">Tujuan</Link>
+          <i id="darkmode" className=" hidden text-[var(--primary)]"></i>
+        </nav>
+      </div>
+      <section
+        id="hero"
+        className=" min-h-[100vh] flex items-center bg-no-repeat relative bg-cover bg-img hero-after"
+      >
+        <main className=" p-[1.4rem 7%] max-w-[60rem]">
+          <h1 className="size-[5rem] text-white shadow-[1px 1px 3px rgba(1,1,3,0.5)] leading-[1.2]">
+            Pulang Sekolah Enaknya <span className="text-[#926d49]">Ngopi</span>{" "}
+            Dulu Gk Sih?
+          </h1>
+          <p className="size-[1.6rem] mt-[0.7rem] leading-[1.3] font-[300]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+            nesciunt modi excepturi vero animi possimus ratione obcaecati
+            incidunt illum aperiam! Explicabo incidunt reiciendis in quidem
+            consequuntur, laboriosam ipsam voluptas veritatis?
+          </p>
+          <Link
+            href="#menu"
+            className=" mt-[1rem] inline-block p-[1rem 3 rem] size-[1.4rem] text-white bg-[var(--primary)] border-r-[0.5rem] shadow-[1px 1px 3px rgba(1,1,3,0.5)]"
           >
-            By{" "}
+            Liat dulu aja
+          </Link>
+        </main>
+      </section>
+      <div id="detail">
+        <section id="tentang" className=" p-[8rem 7% 1.4rem]">
+          <h2 className=" text-center size-[2.6rem] mb-[3rem]">
+            <span className="text-[var(--primary)]">Tentang</span> Kami
+          </h2>
+          <div id="row" className="flex">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+              alt="gambar kami"
+              className=" flex-[1 1 45rem] w-[100%]"
+            ></Image>
+            <section id="content" className="flex-[1 1 35rem] p-[0 1rem]">
+              <h3 className="size-[1.8rem] mb-[1rem]">Siapa Kami?</h3>
+              <div id="content content" className=" size-[1.3rem] font-[300] leading-[1.6] mb-[0.8rem]">
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex,
+                  consequuntur blanditiis, fuga consequatur pariatur, distinctio
+                  suscipit dolor impedit voluptatibus aliquid neque modi?
+                  Accusantium nemo amet nesciunt cumque placeat. Quod, ipsum.
+                </p>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Consectetur odio suscipit, reprehenderit eos blanditiis
+                  aperiam perspiciatis nemo quibusdam laboriosam earum sapiente
+                  temporibus aliquid adipisci laudantium cumque consequuntur at
+                  sed repellat.
+                </p>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section id="menu" className=" p-[8rem 7% 1.4rem]">
+          <h2 className=" text-center size-[2.6rem] mb-[1rem]"><span className="text-[var(--primary)]">Our</span> Menu</h2>
+          <p className=" text-center max-w-[30rem] m-auto font-[300] leading-[1.6] size-[1.3rem]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque sequi dignissimos perferendis placeat enim voluptatibus tempora possimus optio labore nam, necessitatibus architecto, ipsum numquam rem ad provident modi maiores ipsa?</p>
+          <div id="row" className="flex flex-wrap mt-[5rem] justify-center size-[1.8rem]">
+            <div id="menu-card-1" className=" text-center p-[3rem]">
+              <Image alt="kopi" className=" border-r-[50%] w-[30%]"></Image>
+              <h3 id="title" className=" mt-[1rem auto 0.5rem]">Espresso</h3>
+              <p id="price">IDR 20k</p>
+            </div>
+          </div>
+        </section>
+        <div id="tujuan"></div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div id="footer"></div>
     </main>
   );
 }
