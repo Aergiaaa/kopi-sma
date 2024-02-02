ea9d0c3abdbe15b4e3b8eb0f8b056709e2940ccd
Aergiaaa <125899328+Aergiaaa@users.noreply.github.com>
2024-02-02 22:48:56 +0700

    feat: adding about section and masking hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config } */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,7 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["poppins", "sans-serif"],
+      },
       backgroundImage: {
+        "hero": "url('/img/hero-coffee.jpg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
