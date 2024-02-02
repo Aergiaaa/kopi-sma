06eb346d5f4753d1a600330cd8aa58def1132c56
Aergiaaa <125899328+Aergiaaa@users.noreply.github.com>
2024-02-02 11:02:23 +0700

    feat: adding navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,34 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="m-0 p-0 box-border outline-none no-underline scroll-smooth bg-[var(--bg)] text-white">
-      <div
-        id="navbar"
-        className="flex justify-between items-center bg-[rgba(1,1,1,0.8)] top-0 right-0 left-0 z-[9999] fixed border-b-[1px solid #513c28] "
-      >
-        <Link href="#" className=" font-[700] size-[2rem] text-white italic">
+    <main className="bg-[var(--bg)] text-white font-serif m-0 p-0 box-border [outline:none;] [text-decoration:none;] ">
+      <nav className=" navbar">
+        <Link href="#" className=" text-[2rem] font-[700] text-white italic">
           Kopi <span className=" text-[var(--primary)]">Sma</span>.
         </Link>
-        <nav className="text-white inline-block size-[1.2rem] m-[0 1rem] hover:text-[var(--primary)] after:block after:pb-[0.5rem] after:border-b[0.1rem solid var(--primary)] after:scale-x-0 after:transition-[0.2s linear] hover:after:scale-x-[0.5]">
-          <Link href="#tentang">Tentang</Link>
-          <Link href="#menu">Menu</Link>
-          <Link href="#tujuan">Tujuan</Link>
+        <nav className="">
+          <Link
+            href="#tentang"
+            className=" hover:after:transform-scalex-05 navbar-nav after:nav-after hover:text-[var(--primary)]"
+          >
+            Tentang
+          </Link>
+          <Link
+            href="#menu"
+            className="hover:after:transform-scalex-05 navbar-nav after:nav-after hover:text-[var(--primary)]"
+          >
+            Menu
+          </Link>
+          <Link
+            href="#tujuan"
+            className="navbar-nav hover:after:transform-scalex-05 after:nav-after hover:text-[var(--primary)]"
+          >
+            Tujuan
+          </Link>
           <i id="darkmode" className=" hidden text-[var(--primary)]"></i>
         </nav>
-      </div>
-      <section
+      </nav>
+      {/* <section
         id="hero"
         className=" min-h-[100vh] flex items-center bg-no-repeat relative bg-cover bg-img hero-after"
       >
@@ -39,12 +51,12 @@ export default function Home() {
           <Link
             href="#menu"
             className=" mt-[1rem] inline-block p-[1rem 3 rem] size-[1.4rem] text-white bg-[var(--primary)] border-r-[0.5rem] shadow-[1px 1px 3px rgba(1,1,3,0.5)]"
-          >
+            >
             Liat dulu aja
-          </Link>
-        </main>
-      </section>
-      <div id="detail">
+            </Link>
+            </main>
+            </section>
+            <div id="detail">
         <section id="tentang" className=" p-[8rem 7% 1.4rem]">
           <h2 className=" text-center size-[2.6rem] mb-[3rem]">
             <span className="text-[var(--primary)]">Tentang</span> Kami
@@ -78,16 +90,16 @@ export default function Home() {
           <h2 className=" text-center size-[2.6rem] mb-[1rem]"><span className="text-[var(--primary)]">Our</span> Menu</h2>
           <p className=" text-center max-w-[30rem] m-auto font-[300] leading-[1.6] size-[1.3rem]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque sequi dignissimos perferendis placeat enim voluptatibus tempora possimus optio labore nam, necessitatibus architecto, ipsum numquam rem ad provident modi maiores ipsa?</p>
           <div id="row" className="flex flex-wrap mt-[5rem] justify-center size-[1.8rem]">
-            <div id="menu-card-1" className=" text-center p-[3rem]">
-              <Image alt="kopi" className=" border-r-[50%] w-[30%]"></Image>
-              <h3 id="title" className=" mt-[1rem auto 0.5rem]">Espresso</h3>
-              <p id="price">IDR 20k</p>
-            </div>
+          <div id="menu-card-1" className=" text-center p-[3rem]">
+          <Image alt="kopi" className=" border-r-[50%] w-[30%]"></Image>
+          <h3 id="title" className=" mt-[1rem auto 0.5rem]">Espresso</h3>
+          <p id="price">IDR 20k</p>
+          </div>
           </div>
         </section>
         <div id="tujuan"></div>
       </div>
-      <div id="footer"></div>
+      <div id="footer"></div> */}
     </main>
   );
 }
