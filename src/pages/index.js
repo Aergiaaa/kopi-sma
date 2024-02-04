940a7dd4e9db5c88a14c471268c789157ce3da34
Aergiaaa <125899328+Aergiaaa@users.noreply.github.com>
2024-02-04 15:52:09 +0700

    making nav,hero,and about being responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,52 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-[var(--bg)] text-white font-poppins m-0 p-0 box-border [outline:none;] [text-decoration:none;] ">
+    <div className="bg-[var(--bg)] text-white m-0 p-0 box-border [outline:none;] [text-decoration:none;] ">
       <nav className=" navbar">
         <Link href="#" className=" text-[2rem] font-[700] text-white italic">
-          kopi<span className=" text-[var(--primary)]">sekolah</span>.
+          Kopi<span className=" text-[var(--primary)]">Sekolah</span>.
         </Link>
-        <nav className="">
+        <nav className="navbar-detail">
           <Link
+            id="nav-a"
             href="#tentang"
             className=" hover:after:transform-scalex-05 navbar-nav after:nav-after hover:text-[var(--primary)]"
           >
             Tentang
           </Link>
           <Link
+            id="nav-a"
             href="#menu"
             className="hover:after:transform-scalex-05 navbar-nav after:nav-after hover:text-[var(--primary)]"
           >
             Menu
           </Link>
           <Link
+            id="nav-a"
             href="#tujuan"
             className="navbar-nav hover:after:transform-scalex-05 after:nav-after hover:text-[var(--primary)]"
           >
             Tujuan
           </Link>
-          <i id="darkmode" className=" hidden text-[var(--primary)]"></i>
+          <i id="hamburger" className=" hidden text-[var(--primary)]"></i>
         </nav>
       </nav>
 
       <section
         id="hero"
-        className=" h-[972px] flex min-h-[100vh] items-center bg-hero bg-no-repeat bg-cover bg-center relative after:hero-after gradient-mask"
+        className=" flex min-h-[100vh] items-center bg-hero bg-no-repeat bg-cover bg-center relative gradient-mask"
       >
-        <main className=" py-[1.4rem] px-[7%] w-[100%] fixed top-[130px] text-center">
+        <main className=" py-[1.4rem] px-[7%] w-[100%] fixed top-[180px] text-center">
           <h1 className=" hero-content">
             Nikmatnya Secangkir
             <span className="text-[var(--primary2)]"> Kopi </span>
             Setelah Sekolah
           </h1>
-          <p className=" text-[1.6rem] mt-[0.7rem] leading-[1.3] text-[300] [text-shadow: 1px 1px 3px rgba(1,1,3,0.5);] ">
-            Lorem ipsum dolor sit.
-          </p>
         </main>
       </section>
       <div id="detail">
         <section id="tentang" className=" pt-[8rem] px-[7%] pb-[1.4rem]">
-          <h2 className=" text-justify text-[2.6rem] mb-[3rem] font-[300]">
+          <h2 className=" text-center text-[2.6rem] mb-[3rem] font-[300]">
             <span className="text-[var(--primary)]">Tentang</span> Kami
           </h2>
           <div id="row" className="flex">
@@ -56,30 +56,25 @@ export default function Home() {
               id="content"
               className="flex-grow-[1] flex-shrink-[1] basis-[35rem] py-[0] px-[1rem]"
             >
-              <h3 className="text-[1.8rem] mb-[1rem] font-bold italic">
-                kopi<span className=" text-[var(--primary2)]">sekolah</span>
-              </h3>
               <div
-                id="content content"
-                className=" text-[1.3rem] leading-[1.6] mb-[0.8rem]"
+                id="content-content"
+                className=" text-[1.3rem] font-[100] leading-[1.6] mb-[0.8rem] pr-[1rem] "
               >
                 <p>
                   {" "}
                   Kami berdedikasi untuk menciptakan kopi yang lezat dan
                   berkualitas tetapi dengan harga yang sangat terjangkau. Tujuan
                   kami adalah agar setiap orang, terutama generasi muda, bisa
-                  menikmati rasa kopi yang enak setelah seharian belajar. Dengan
-                  menawarkan kopi yang murah namun tidak mengorbankan rasa, kami
-                  berharap bisa membawa ketenangan setelah beraktivitas.
+                  menikmati rasa kopi yang enak setelah seharian belajar.
                 </p>
               </div>
             </section>
             <Image
               alt="gambar kami"
-              className=" flex-grow-[1] flex-shrink-[1] basis-[45rem] w-[100%] "
+              className="about-img"
               src="/img/tyler-nix-yGb2igKldYg-unsplash.jpg"
               width="400"
-              height="400"
+              height="300"
             ></Image>
           </div>
         </section>
